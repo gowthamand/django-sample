28e82472bb4dad723f0d86205bc950ba100a79c9
Gowthaman <gowthaman.nkl1@gmail.com>
2026-02-23 12:48:20 +0530

    Fix CSS loading: enable DEBUG mode and update vercel.json static routes

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_jp+i(q1%hu^84h%ft$(27t*2%m+)@w+b_q0wan69kb3m!=+y0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
